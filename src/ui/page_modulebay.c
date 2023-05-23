418c2b58d69526a0d3286a8b4fc72991230b8363
Paul Kendall <pkendall64@gmail.com>
2023-05-23 14:50:58 +1200

    Change module-bay settings to use dropdowns

--- src/ui/page_modulebay.c
+++ src/ui/page_modulebay.c
@@ -9,48 +9,55 @@
 #include "ui/ui_style.h"
 
 static lv_obj_t *cont;
-static btn_group_t btn_group_module_type;
-static btn_group_t btn_group_rapidfire;
-static btn_group_t btn_group_steadyview;
+static lv_obj_t *btn_module_type;
+static lv_obj_t *lbl_module_mode;
+static lv_obj_t *btn_module_mode;
+static lv_obj_t *back_btn1;
 static lv_obj_t *back_btn2;
-static lv_obj_t *back_btn3;
-static lv_obj_t *back_btn4;
+
+static lv_obj_t *page_item_focused = NULL;
 
 static lv_coord_t col_dsc[] = {160, 120, 240, 240, 240, 100, LV_GRID_TEMPLATE_LAST};
 static lv_coord_t row_dsc[] = {60, 60, 60, 60, 60, 60, 60, 60, 60, 60, LV_GRID_TEMPLATE_LAST};
-lv_obj_t *label_cell_count;
 
-void show_module_settings() {
-    switch (btn_group_get_sel(&btn_group_module_type)) {
+void show_module_settings(int mode) {
+    switch (lv_dropdown_get_selected(btn_module_type)) {
     case MODULE_RAPIDFIRE:
-        btn_group_show(&btn_group_rapidfire, true);
-        btn_group_show(&btn_group_steadyview, false);
-        lv_obj_add_flag(back_btn2, LV_OBJ_FLAG_HIDDEN);
-        lv_obj_clear_flag(back_btn3, LV_OBJ_FLAG_HIDDEN);
-        lv_obj_add_flag(back_btn4, LV_OBJ_FLAG_HIDDEN);
-        btn_group_set_sel(&btn_group_rapidfire, g_setting.module.setting);
-        pp_modulebay.p_arr.max = 4;
+        lv_dropdown_set_options(btn_module_mode, "legacy\nrapidFIRE 1\nrapidFIRE 2");
+        lv_dropdown_set_selected(btn_module_mode, mode);
+        lv_obj_clear_flag(btn_module_mode, LV_OBJ_FLAG_HIDDEN);
+        lv_obj_clear_flag(lbl_module_mode, LV_OBJ_FLAG_HIDDEN);
+        lv_obj_add_flag(back_btn1, LV_OBJ_FLAG_HIDDEN);
+        lv_obj_clear_flag(back_btn2, LV_OBJ_FLAG_HIDDEN);
+        pp_modulebay.p_arr.max = 3;
         break;
 
     case MODULE_STEADYVIEW:
     case MODULE_STEADYVIEW_X:
-        btn_group_show(&btn_group_rapidfire, false);
+        lv_dropdown_set_options(btn_module_mode, "Diversity\nMix 1\nMix 2\nMix 3");
+        lv_dropdown_set_selected(btn_module_mode, mode);
         //steadyview need repower to switch mode, old HW revision can not power cycle analog module
-        btn_group_show(&btn_group_steadyview, getHwRevision() >= HW_REV_2 || btn_group_get_sel(&btn_group_module_type) == MODULE_STEADYVIEW_X);
-        lv_obj_add_flag(back_btn2, LV_OBJ_FLAG_HIDDEN);
-        lv_obj_add_flag(back_btn3, LV_OBJ_FLAG_HIDDEN);
-        lv_obj_clear_flag(back_btn4, LV_OBJ_FLAG_HIDDEN);
-        btn_group_set_sel(&btn_group_steadyview, g_setting.module.setting);
-        pp_modulebay.p_arr.max = 5;
+        if (getHwRevision() >= HW_REV_2 || lv_dropdown_get_selected(btn_module_type) == MODULE_STEADYVIEW_X) {
+            lv_obj_clear_flag(btn_module_mode, LV_OBJ_FLAG_HIDDEN);
+            lv_obj_clear_flag(lbl_module_mode, LV_OBJ_FLAG_HIDDEN);
+            lv_obj_add_flag(back_btn1, LV_OBJ_FLAG_HIDDEN);
+            lv_obj_clear_flag(back_btn2, LV_OBJ_FLAG_HIDDEN);
+            pp_modulebay.p_arr.max = 3;
+        } else {
+            lv_obj_add_flag(btn_module_mode, LV_OBJ_FLAG_HIDDEN);
+            lv_obj_add_flag(lbl_module_mode, LV_OBJ_FLAG_HIDDEN);
+            lv_obj_clear_flag(back_btn1, LV_OBJ_FLAG_HIDDEN);
+            lv_obj_add_flag(back_btn2, LV_OBJ_FLAG_HIDDEN);
+            pp_modulebay.p_arr.max = 2;
+        }
         break;
 
     default:
-        btn_group_show(&btn_group_rapidfire, false);
-        btn_group_show(&btn_group_steadyview, false);
-        lv_obj_clear_flag(back_btn2, LV_OBJ_FLAG_HIDDEN);
-        lv_obj_add_flag(back_btn3, LV_OBJ_FLAG_HIDDEN);
-        lv_obj_add_flag(back_btn4, LV_OBJ_FLAG_HIDDEN);
-        pp_modulebay.p_arr.max = 3;
+        lv_obj_add_flag(btn_module_mode, LV_OBJ_FLAG_HIDDEN);
+        lv_obj_add_flag(lbl_module_mode, LV_OBJ_FLAG_HIDDEN);
+        lv_obj_clear_flag(back_btn1, LV_OBJ_FLAG_HIDDEN);
+        lv_obj_add_flag(back_btn2, LV_OBJ_FLAG_HIDDEN);
+        pp_modulebay.p_arr.max = 2;
         break;
     }
 }
@@ -81,47 +88,70 @@ static lv_obj_t *page_create(lv_obj_t *parent, panel_arr_t *arr) {
     create_select_item(arr, cont);
 
     // create menu entries
-    create_btn_group_item2(&btn_group_module_type, cont, 5, "Type", "Unknown", "Switch", "rapidFIRE", "Steadyview", "Steadyview-X", NULL, 0);
-    btn_group_set_sel(&btn_group_module_type, g_setting.module.type);
+    create_label_item(cont, "Type", 1, 0, 1);
+    btn_module_type = create_dropdown_item(cont, "Unknown\nSwitch\nrapidFIRE\nSteadyview\nSteadyview-X", 2, 0);
+    lv_obj_set_size(btn_module_type, 240, 40);
 
-    // rapidFIRE Mode - rapidFIRE 1, rapidFIRE 2, Legacy
-    create_btn_group_item(&btn_group_rapidfire, cont, 3, "Mode", "legacy", "rapidFIRE  1", "rapidFIRE 2", NULL, 2);
-    btn_group_set_sel(&btn_group_rapidfire, g_setting.module.setting);
-
-    // Mix Mode - Diversity, Mix 1, Mix 2, Mix 3
-    create_btn_group_item2(&btn_group_steadyview, cont, 4, "Mode", "Diversity", "Mix 1", "Mix 2", "Mix 3", NULL, NULL, 2);
-    btn_group_set_sel(&btn_group_steadyview, g_setting.module.setting);
+    // settings/mode menu entries
+    lbl_module_mode = create_label_item(cont, "Mode", 1, 1, 1);
+    btn_module_mode = create_dropdown_item(cont, "None", 2, 1);
+    lv_obj_set_size(btn_module_mode, 240, 40);
 
     // back buttons in possible locations
+    back_btn1 = create_label_item(cont, "< Back", 1, 1, 1);
     back_btn2 = create_label_item(cont, "< Back", 1, 2, 1);
-    back_btn3 = create_label_item(cont, "< Back", 1, 3, 1);
-    back_btn4 = create_label_item(cont, "< Back", 1, 4, 1);
 
-    show_module_settings();
+    lv_dropdown_set_selected(btn_module_type, g_setting.module.type);
+    show_module_settings(g_setting.module.setting);
 
     return page;
+}
+
+static void page_on_roller(uint8_t key) {
+    if (page_item_focused) {
+        lv_obj_remove_style(page_item_focused, &style_dropdown, LV_PART_MAIN);
+
+        if (key == DIAL_KEY_UP) {
+            uint32_t evt = LV_KEY_DOWN;
+            lv_event_send(page_item_focused, LV_EVENT_KEY, &evt);
+        } else if (key == DIAL_KEY_DOWN) {
+            uint32_t evt = LV_KEY_UP;
+            lv_event_send(page_item_focused, LV_EVENT_KEY, &evt);
+        }
+    }
 }
 
 static void page_on_click(uint8_t key, int sel) {
     switch (sel) {
 
     case 0:
+        if (!page_item_focused) {
+            lv_obj_t *list = lv_dropdown_get_list(btn_module_type);
+            lv_dropdown_open(btn_module_type);
+            lv_obj_add_style(list, &style_dropdown, LV_PART_MAIN);
+            lv_obj_set_style_text_color(list, lv_color_make(0, 0, 0), LV_PART_SELECTED | LV_STATE_CHECKED);
+            page_item_focused = btn_module_type;
+            pp_modulebay.p_arr.max = 0; // we are now in control of the roller
+        } else {
+            lv_event_send(btn_module_type, LV_EVENT_RELEASED, NULL);
+            page_item_focused = NULL;
+            show_module_settings(g_setting.module.type == lv_dropdown_get_selected(btn_module_type) ? g_setting.module.setting : 0);
+        }
+        break;
+
     case 1:
-        btn_group_toggle_sel(&btn_group_module_type);
-        g_setting.module.setting = 0;
-        show_module_settings();
-        break;
-
-    case 2:
-        if (btn_group_get_sel(&btn_group_module_type) == MODULE_RAPIDFIRE)
-            btn_group_toggle_sel(&btn_group_rapidfire);
-        if (btn_group_get_sel(&btn_group_module_type) == MODULE_STEADYVIEW || btn_group_get_sel(&btn_group_module_type) == MODULE_STEADYVIEW_X)
-            btn_group_toggle_sel(&btn_group_steadyview);
-        break;
-
-    case 3:
-        if (btn_group_get_sel(&btn_group_module_type) == MODULE_STEADYVIEW || btn_group_get_sel(&btn_group_module_type) == MODULE_STEADYVIEW_X)
-            btn_group_toggle_sel(&btn_group_steadyview);
+        if (!page_item_focused) {
+            lv_obj_t *list = lv_dropdown_get_list(btn_module_mode);
+            lv_dropdown_open(btn_module_mode);
+            lv_obj_add_style(list, &style_dropdown, LV_PART_MAIN);
+            lv_obj_set_style_text_color(list, lv_color_make(0, 0, 0), LV_PART_SELECTED | LV_STATE_CHECKED);
+            page_item_focused = btn_module_mode;
+            pp_modulebay.p_arr.max = 0; // we are now in control of the roller
+        } else {
+            lv_event_send(btn_module_mode, LV_EVENT_RELEASED, NULL);
+            page_item_focused = NULL;
+            show_module_settings(lv_dropdown_get_selected(btn_module_mode));
+        }
         break;
 
     default:
@@ -130,17 +160,14 @@ static void page_on_click(uint8_t key, int sel) {
 }
 
 static void page_exit() {
-    int module_type = btn_group_get_sel(&btn_group_module_type);
+    int module_type = lv_dropdown_get_selected(btn_module_type);
     if (g_setting.module.type != module_type) {
         module_close();
         module_type = g_setting.module.type;
     }
-    g_setting.module.type = btn_group_get_sel(&btn_group_module_type);
+    g_setting.module.type = lv_dropdown_get_selected(btn_module_type);
     ini_putl("module", "type", g_setting.module.type, SETTING_INI);
-    if (g_setting.module.type == MODULE_RAPIDFIRE)
-        g_setting.module.setting = btn_group_get_sel(&btn_group_rapidfire);
-    if (g_setting.module.type == MODULE_STEADYVIEW || g_setting.module.type == MODULE_STEADYVIEW_X)
-        g_setting.module.setting = btn_group_get_sel(&btn_group_steadyview);
+    g_setting.module.setting = lv_dropdown_get_selected(btn_module_mode);
     ini_putl("module", "setting", g_setting.module.setting, SETTING_INI);
     if (module_type == g_setting.module.type) {
         module_set_mode();
@@ -152,14 +179,14 @@ static void page_exit() {
 page_pack_t pp_modulebay = {
     .p_arr = {
         .cur = 0,
-        .max = 3,
+        .max = 0,
     },
 
     .name = "Module Bay",
     .create = page_create,
     .enter = NULL,
     .exit = page_exit,
-    .on_roller = NULL,
+    .on_roller = page_on_roller,
     .on_click = page_on_click,
     .on_right_button = NULL,
 };
