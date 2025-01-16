950c7c5e016d26448ad3b542af892861767c9321
Paul Kendall <pkendall64@gmail.com>
2025-01-16 15:50:11 +1300

    Adjust the rapidFIRE SPI timing

--- src/module/rapidfire.c
+++ src/module/rapidfire.c
@@ -3,17 +3,17 @@
 #include "core/common.hh"
 #include "core/settings.h"
 
-#include "softspi.h"
 #include "rapidfire.h"
+#include "softspi.h"
 
-#define SPAM_COUNT          3
+#define SPAM_COUNT 3
 
-#define RF_API_DIR_GRTHAN   '>'
-#define RF_API_DIR_EQUAL    '='
-#define RF_API_BEEP_CMD     'S'
-#define RF_API_BAND_CMD     'B'
-#define RF_API_CHANNEL_CMD  'C'
-#define RF_API_MODE_CMD     'D'
+#define RF_API_DIR_GRTHAN  '>'
+#define RF_API_DIR_EQUAL   '='
+#define RF_API_BEEP_CMD    'S'
+#define RF_API_BAND_CMD    'B'
+#define RF_API_CHANNEL_CMD 'C'
+#define RF_API_MODE_CMD    'D'
 
 static int current_channel = -1;
 
@@ -26,15 +26,15 @@ static void rapidfire_set_channel(int index) {
     uint8_t channel = index % 8;
 
     send_band(band);
-	usleep(100000);
+    usleep(100000);
     send_channel(channel);
 
     current_channel = index;
 }
 
 static void rapidfire_set_mode() {
-    rapidfire_set_channel(g_setting.module.channel-1);
-	usleep(100000);
+    rapidfire_set_channel(g_setting.module.channel - 1);
+    usleep(100000);
     send_mode(g_setting.module.setting);
 }
 
@@ -61,32 +61,31 @@ static void rapidfire_close() {
 // CRC function for IMRC rapidfire API
 // Input: byte array, array length
 // Output: crc byte
-static uint8_t crc8(uint8_t* buf, uint8_t bufLen) {
-  uint32_t sum = 0;
-  for (uint8_t i = 0; i < bufLen; ++i) {
-    sum += buf[i];
-  }
-  return sum & 0xFF;
+static uint8_t crc8(uint8_t *buf, uint8_t bufLen) {
+    uint32_t sum = 0;
+    for (uint8_t i = 0; i < bufLen; ++i) {
+        sum += buf[i];
+    }
+    return sum & 0xFF;
 }
 
-static void send_spi(uint8_t* buf, uint8_t len) {
+static void send_spi(uint8_t *buf, uint8_t len) {
     softspi_set_pin(SOFTSPI_CS, 0);
-    usleep(10);
+    usleep(16);
 
     for (int i = 0; i < len; ++i) {
         uint8_t byte = buf[i];
         for (uint8_t k = 0; k < 8; k++) {
-            softspi_set_pin(SOFTSPI_CLK, 0);
-            usleep(10);
             softspi_set_pin(SOFTSPI_DAT, byte & 0x80);
-            usleep(10);
             softspi_set_pin(SOFTSPI_CLK, 1);
-            usleep(10);
+            usleep(9);
+            softspi_set_pin(SOFTSPI_CLK, 0);
+            usleep(8);
             byte <<= 1;
         }
     }
+    usleep(16);
     softspi_set_pin(SOFTSPI_CS, 1);
-    softspi_set_pin(SOFTSPI_CLK, 0);
 }
 
 static void send_band(uint8_t band) {
@@ -95,17 +94,17 @@ static void send_band(uint8_t band) {
     uint8_t imrcBand = band_map[band];
 
     uint8_t cmd[5];
-    cmd[0] = RF_API_BAND_CMD;       // 'B'
-    cmd[1] = RF_API_DIR_EQUAL;      // '='
-    cmd[2] = 0x01;                  // len
-    cmd[3] = imrcBand;              // temporarily set byte 4 to band for crc calc
-    cmd[3] = crc8(cmd, 4);          // reset byte 4 to crc
-    cmd[4] = imrcBand;              // assign band to correct byte 5
+    cmd[0] = RF_API_BAND_CMD;  // 'B'
+    cmd[1] = RF_API_DIR_EQUAL; // '='
+    cmd[2] = 0x01;             // len
+    cmd[3] = imrcBand;         // temporarily set byte 4 to band for crc calc
+    cmd[3] = crc8(cmd, 4);     // reset byte 4 to crc
+    cmd[4] = imrcBand;         // assign band to correct byte 5
 
     // rapidfire sometimes misses pkts, so send each one 3x
     for (int i = 0; i < SPAM_COUNT; i++) {
         send_spi(cmd, 5);
-        usleep(1000);
+        usleep(10000);
     }
 }
 
@@ -114,33 +113,33 @@ static void send_channel(uint8_t channel) {
     channel++;
 
     uint8_t cmd[5];
-    cmd[0] = RF_API_CHANNEL_CMD;    // 'C'
-    cmd[1] = RF_API_DIR_EQUAL;      // '='
-    cmd[2] = 0x01;                  // len
-    cmd[3] = channel;               // temporarily set byte 4 to channel for crc calc
-    cmd[3] = crc8(cmd, 4);          // reset byte 4 to crc
-    cmd[4] = channel;               // assign channel to correct byte 5
+    cmd[0] = RF_API_CHANNEL_CMD; // 'C'
+    cmd[1] = RF_API_DIR_EQUAL;   // '='
+    cmd[2] = 0x01;               // len
+    cmd[3] = channel;            // temporarily set byte 4 to channel for crc calc
+    cmd[3] = crc8(cmd, 4);       // reset byte 4 to crc
+    cmd[4] = channel;            // assign channel to correct byte 5
 
     // rapidfire sometimes misses pkts, so send each one 3x
     for (int i = 0; i < SPAM_COUNT; i++) {
         send_spi(cmd, 5);
-        usleep(1000);
+        usleep(10000);
     }
 }
 
 static void send_mode(uint8_t mode) {
     uint8_t cmd[5];
-    cmd[0] = RF_API_MODE_CMD;       // 'D'
-    cmd[1] = RF_API_DIR_EQUAL;      // '='
-    cmd[2] = 0x01;                  // len
-    cmd[3] = mode;                  // temporarily set byte 4 to mode for crc calc
-    cmd[3] = crc8(cmd, 4);          // reset byte 4 to crc
-    cmd[4] = mode;                  // assign mode to correct byte 5
+    cmd[0] = RF_API_MODE_CMD;  // 'D'
+    cmd[1] = RF_API_DIR_EQUAL; // '='
+    cmd[2] = 0x01;             // len
+    cmd[3] = mode;             // temporarily set byte 4 to mode for crc calc
+    cmd[3] = crc8(cmd, 4);     // reset byte 4 to crc
+    cmd[4] = mode;             // assign mode to correct byte 5
 
     // rapidfire sometimes misses pkts, so send each one 3x
     for (int i = 0; i < SPAM_COUNT; i++) {
         send_spi(cmd, 5);
-        usleep(1000);
+        usleep(10000);
     }
 }
 
@@ -164,5 +163,5 @@ module_def_t rapidfire_module = {
     rapidfire_init,
     rapidfire_close,
     rapidfire_set_channel,
-    rapidfire_set_mode
+    rapidfire_set_mode,
 };
